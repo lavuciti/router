9347b9c1a9541edbe37f8ea2d38dcf979a349f8e
lavuciti <81195186+lavuciti@users.noreply.github.com>
2021-06-22 22:17:09 +0200

    Add files via upload

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,9 @@ import { Link, NavLink } from "react-router-dom";
 //import "./Navbar.css";
 import styles from "./Navbar.module.css";
 
+//ovde su dva primera sa NavLink. Ovde je primer sa Navbar.module.css i u njima mora da se u javascriptu promeni activnaclasa u styles.active inace nece da radi
+//drugi primer je obican Navbar.css. On ce raditi bez menjanja klase i u ovom slucaju mora da se izbrise activeClassName={styles.active}
+
 const Navbar = () => {
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
