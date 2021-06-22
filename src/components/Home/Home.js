691b2f58a6e1c45eec2d60f5561fbefaa8d9916d
lavuciti <lavrajic@yahoo.com>
2021-06-22 22:42:45 +0200

    modified home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+//ovde smo uradili redirekciju. Kada kliknemo na Home za 5 sekundi predje na About.
+//Zanimljivo je da se consol.log(props) i da se vidi u konzoli koje sve objekte sadrzi. Ima dosta korisnih stvari ovo je samo jedna od njih
 const Home = (props) => {
         setTimeout(function(){
             props.history.push("/about")
